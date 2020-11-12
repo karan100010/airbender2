9bed04764c202d3dd90c185eb39dc1b69b7bb6ac
karan100010 <karanjoshi1993@gmail.com>
2020-11-12 18:32:38 +0530

    single process mulithreded

--- wsgi.py
+++ wsgi.py
@@ -1,5 +1,5 @@
 from app.main import app 
-from app.base import *
+
 if __name__ == '__main__':
     print("Starting python app")
-    app.run(host='0.0.0.0',port=8080,processes=8,threaded=False)
+    app.run(host='0.0.0.0',port=8080)
